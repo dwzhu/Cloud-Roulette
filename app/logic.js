b0a574dfef0076413890c2a9a5341d456d35b654
David Wei Zhu <Davidyyzz@Davids-MacBook-Pro-5.local>
2017-10-28 01:48:31 -0700

    gamefunctions

--- app/logic.js
+++ app/logic.js
@@ -1,22 +1,22 @@
+var firstTwelve = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var secondTwelve = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var thirdTwelve = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
+var topRow = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
+var midRow = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
+var botRow = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
+var red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 21, 23, 25, 27, 28, 30, 32, 34, 36];
+var black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 19, 20, 22, 24, 26, 29, 31, 33, 35];
+var zero = [0]
+var firstHalf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var secondHalf = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
+var chipsPlaced = [];
+var multiplier = chipsPlaced.length;
+var prevSpins = [];
+var redSpins = [];
+var blackSpins = [];
+var balance = 100;
 // define win conditions
 $(document).ready(function() {
-    var firstTwelve = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var secondTwelve = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-    var thirdTwelve = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-    var topRow = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
-    var midRow = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
-    var botRow = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
-    var red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 21, 23, 25, 27, 28, 30, 32, 34, 36];
-    var black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 19, 20, 22, 24, 26, 29, 31, 33, 35];
-    var zero = [0]
-    var firstHalf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-    var secondHalf = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-    var chipsPlaced = [];
-    var multiplier = chipsPlaced.length;
-    var prevSpins = [];
-    var redSpins = [];
-    var blackSpins = [];
-    var balance = 10;
 
     $('select').material_select();
     $('select').on("click change", function() {
@@ -371,6 +371,20 @@ $(document).ready(function() {
                     balance = balance + betAmt * 2
                 }
             };
+            if (chipsPlaced[i] === "even") {
+                if (lastSpin % 2 === 0) {
+                    if (lastSpin !== 0) {
+                        balance = balance + betAmt * 2
+                    }
+                }
+            };
+            if (chipsPlaced[i] === "odd") {
+                if (lastSpin % 2 === 1) {
+                    if (lastSpin !== 0) {
+                        balance = balance + betAmt * 2
+                    }
+                }
+            };
         };
 
         if (red.includes(lastSpin)) {
@@ -388,22 +402,26 @@ $(document).ready(function() {
         $('#lastSpin').html(lastSpin);
 
         if (black.includes(lastSpin)) {
-            var lastSpinDisplay = "<a class='black-text col s1'>" + lastSpin + " </a>";
+            var lastSpinDisplay = "<a class='black-text col s2'>" + lastSpin + " </a>";
             $('#spinDisplay').append(lastSpinDisplay)
         };
 
         if (red.includes(lastSpin)) {
-            var lastSpinDisplay = "<a class='red-text col s1'>" + lastSpin + " </a> ";
+            var lastSpinDisplay = "<a class='red-text col s2'>" + lastSpin + " </a> ";
             $('#spinDisplay').append(lastSpinDisplay)
         };
 
         if (zero.includes(lastSpin)) {
-            var lastSpinDisplay = "<a class='green-text col s1'>" + lastSpin + " </a> ";
+            var lastSpinDisplay = "<a class='green-text col s2'>" + lastSpin + " </a> ";
             $('#spinDisplay').append(lastSpinDisplay)
         };
 
         postBalance = balance;
         payout = postBalance - prevBalance;
+
+        if (payout === 0) {
+            $('#payout').html("<a class='black-text'>" + (Math.round(payout * 10000000) / 10000000) + "</a>")
+        }
 
         if (payout < 0) {
             $('#payout').html("<a class='red-text'>" + (Math.round(payout * 10000000) / 10000000) + "</a>")
@@ -414,9 +432,19 @@ $(document).ready(function() {
         };
 
         $('#balance').html((Math.round(balance * 10000000) / 10000000));
-        console.log("Payout: " + (Math.round(payout * 10000000) / 10000000))
-        console.log("Current Balance: " + (Math.round(balance * 10000000) / 10000000))
+        console.log("Bet Amount: " + betAmt + ", TotalBet: " + Math.round(totalBet * 10000000) / 10000000)
+        console.log("Payout: " + (Math.round(payout * 10000000) / 10000000) + ", Current Balance: " + (Math.round(balance * 10000000) / 10000000));
+        console.log("Last Spin: " + lastSpin)
     });
+
+    // #('#autoSpin').on("click", function() {
+    //     var myVar = setInterval(spinFunction, 1000);
+
+    //     function spinFunction() {
+    //         var d = new Date();
+    //         document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    //     }
+    // })
 
     console.log("Javascript connected!")
 });
